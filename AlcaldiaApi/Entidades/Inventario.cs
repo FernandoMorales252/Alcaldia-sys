51d210198d9aeb34680f504293b6512c9311e706
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-09-09 14:23:21 -0600

    Entidad de Inveentario creada

---
+++ AlcaldiaApi/Entidades/Inventario.cs
@@ -0,0 +1,8 @@
+namespace AlcaldiaApi.Entidades
+{
+    public class TipoInventario
+    {
+        public int Id_tipo_inventario { get; set; }
+        public string Nombre_tipo { get; set; } = "";
+    }
+}
