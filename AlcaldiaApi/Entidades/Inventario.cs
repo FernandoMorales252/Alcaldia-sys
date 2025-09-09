475d62228c26b8c44c718e65c0729302a618352e
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-09-09 14:43:56 -0600

    cambio

--- AlcaldiaApi/Entidades/Inventario.cs
+++ AlcaldiaApi/Entidades/Inventario.cs
@@ -1,8 +1,20 @@
 namespace AlcaldiaApi.Entidades
 {
-    public class TipoInventario
+    public class Inventario
     {
-        public int Id_tipo_inventario { get; set; }
-        public string Nombre_tipo { get; set; } = "";
+        public int Id_inventario { get; set; }
+        public string Codigo_item { get; set; } = "";
+        public string Nombre_item { get; set; } = "";
+        public string Descripcion { get; set; } = "";
+        public int Cantidad { get; set; }
+        public decimal Precio_unitario { get; set; }
+        public DateTime Fecha_ingreso { get; set; }
+        public int Estado { get; set; }
+
+        public int CategoriaId { get; set; }
+        public CategoriaInventario Categoria { get; set; } = null!;
+
+        public int UbicacionId { get; set; }
+        public UbicacionInventario Ubicacion { get; set; } = null!;
     }
 }
