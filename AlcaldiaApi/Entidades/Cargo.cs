56f0faf17750e3ab1803c0b9ed6be9050b21c802
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-09 15:15:14 -0600

    Crear entidades de Cargo & Empleado

---
+++ AlcaldiaApi/Entidades/Cargo.cs
@@ -0,0 +1,10 @@
+namespace AlcaldiaApi.Entidades
+{
+    public class Cargo
+    {
+        public int Id_Cargo { get; set; }
+        public string Nombre_cargo { get; set; } = "";
+        public string Descripcion { get; set; } = "";
+
+    }
+}
