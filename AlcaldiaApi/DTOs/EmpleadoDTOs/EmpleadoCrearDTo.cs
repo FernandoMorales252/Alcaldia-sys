f97591fe8ecdab5abb97ca2ccf20edd3b8ff8130
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-11 14:34:35 -0600

    DTOs de empleado creados

---
+++ AlcaldiaApi/DTOs/EmpleadoDTOs/EmpleadoCrearDTo.cs
@@ -0,0 +1,14 @@
+﻿using AlcaldiaApi.Entidades;
+
+namespace AlcaldiaApi.DTOs.EmpleadoDTOs
+{
+    public class EmpleadoCrearDTo
+    {
+        public string Nombre { get; set; } = "";
+        public string Apellido { get; set; } = "";
+        public DateTime Fecha_contratacion { get; set; }
+        public int Estado { get; set; }
+        public int CargoId { get; set; }
+        public Cargo Cargo { get; set; } = null!;
+    }
+}
--- AlcaldiaApi/obj/Debug/net8.0/AlcaldiaApi.AssemblyInfo.cs
+++ AlcaldiaApi/obj/Debug/net8.0/AlcaldiaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlcaldiaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70478673da5fee1409b816f220cc73fc440544a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e90455159311064ad1295285c0c4fc90daf9b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlcaldiaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlcaldiaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
