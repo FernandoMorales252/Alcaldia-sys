377e79df01c21e467a0fd99a61195192c5b96e65
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-09-22 19:16:01 -0300

    Entidades de proyecto

--- AlcaldiaApi/Entidades/Proyecto.cs
+++ AlcaldiaApi/Entidades/Proyecto.cs
@@ -1,21 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AlcaldiaApi.Entidades
 {
+    [Table("proyecto")]
     public class Proyecto
     {
+        [Key]
+        [Column("id_proyecto")]
         public int Id_proyecto { get; set; }
-
+        [Column("nombre")]
         public string Nombre { get; set; } = "";
-
+        [Column("descripcion")]
         public string Descripcion { get; set; } = "";
-
+        [Column("fecha_inicio")]
         public DateTime Fecha_inicio { get; set; }
-
-        public DateTime? Fecha_fin { get; set; }
-
+        [Column("fecha_fin")]
+        public DateTime Fecha_fin { get; set; }
+        [Column("presupuesto")]
         public decimal Presupuesto { get; set; }
-
+        [Column("estado")]
         public string Estado { get; set; } = "";
-
+        [Column("id_municipio")]
         public int MunicipioId { get; set; }
 
         public Municipio Municipio { get; set; } = null!;
